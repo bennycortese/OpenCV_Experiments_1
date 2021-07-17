047e66310f0a4e4bfa687d7c42b24401a27db8c5
benny <bennycortese@gmail.com>
2021-07-16 22:42:42 -0400

    Changed application to basically just run a song

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import cv2
+import pygame
 from camera import Camera
 
+pygame.mixer.init()
+song = pygame.mixer.Sound("Background Music.wav")
+song.play(-1)
 while True:
-    video_uno = Camera()
-    success, img = video_uno.return_cap().read()
-    cv2.imshow("Video", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    pass
+
+#while True:
+#    video_uno = Camera()
+#    success, img = video_uno.return_cap().read()
+#    cv2.imshow("Video", img)
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#        break
     #TODO - figure out why this is erroring
-    
-    #TODO -- Figure out what we're trying to accomplish here
-    
-    #TODO -- Try to understand why we're doing this
